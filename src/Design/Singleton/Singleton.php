f9daf4f986e4e58742cf7a3b297583aa8ed7add4
chenishr <chenishr@gmail.com>
2017-10-31 16:58:28 +0800

    PHP 是单进程的，创建单例对象不需要加锁

--- src/Design/Singleton/Singleton.php
+++ src/Design/Singleton/Singleton.php
@@ -4,32 +4,27 @@ namespace Design\Singleton;
 /*
  * 单例模式
  * 
- * 创建对象时需要加锁，防止高并发创建多个对象
  * 防止被克隆
  * 防止反序列化
  */ 
 class Singleton{
     // 静态单例对象
     protected static $obj;
+    public $name;
 
     // 私有的构造函数
-    private function __construct(){
+    private function __construct($name){
+        $this->name = $name;
+    }
 
+    public function getName(){
+        return $this->name;
     }
 
     // 创建单例对象
-    public static function getInstance(){
+    public static function getInstance($name){
         if(null === self::$obj){
-            // lock 阻塞
-            $fp = fopen('/tmp/singleton_lock.txt','w+');
-            if(flock($fp,LOCK_EX)){
-                // 再判断
-                if(null === self::$obj){
-                    self::$obj = new Singleton();
-                }
-                // unlock
-                flock($fp,LOCK_UN);
-            }
+            self::$obj = new Singleton($name);
         }
 
         return self::$obj;
